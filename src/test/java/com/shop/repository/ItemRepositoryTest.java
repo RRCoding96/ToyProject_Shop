592b2e8ac747d37f33f7a77ed9df4ae6046d8654
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-03-27 23:31:45 +0900

    test: Item 이름으로 조회하는 test 작성
    Item 이름으로 조회하는 test 작성

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,20 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -36,5 +51,16 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
 
         assertThat(item).isEqualTo(savedItem);
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for(Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
