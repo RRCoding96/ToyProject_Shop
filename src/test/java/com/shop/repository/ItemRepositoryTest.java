6713e3d16de4b3c2de6b47860fe338093d60462a
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-03-27 23:36:08 +0900

    test: Item 이름 or 상세 설명으로 조회하는 기능 test 작성
    Item 이름 or 상세 설명으로 조회하는 기능 test 작성

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -59,6 +59,17 @@ class ItemRepositoryTest {
         this.createItemList();
 
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
         for(Item item : itemList) {
             System.out.println(item.toString());
         }
